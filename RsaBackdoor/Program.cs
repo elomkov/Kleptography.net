1913e8e42d18f4d502096396c0c4541c376e1358
alexey.ermishkin <alexey.ermishkin@engine6.com>
2015-01-20 15:14:30 +0500

    RSA Backdoor PoC

---
+++ RsaBackdoor/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Chaos.NaCl;
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.Security;
+using RsaBackdoor.Backdoor;
+
+namespace RsaBackdoor
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var backdoorEngine = new BackdoorEngine();
+			var randomKeyPair = backdoorEngine.BuildRandomKey();
+
+			var payload = backdoorEngine.ExtractPayload((RsaKeyParameters) randomKeyPair.Public);
+
+			var restoredKey = backdoorEngine.BuildKeyFromPayload(payload);
+
+			var initialParams = ((RsaPrivateCrtKeyParameters)randomKeyPair.Private);
+			var restoredParams = ((RsaPrivateCrtKeyParameters)restoredKey.Private);
+			Console.WriteLine(initialParams.P.Equals(restoredParams.P) && initialParams.Q.Equals(restoredParams.Q));
+			Console.ReadKey();
+		}
+	}
+}
